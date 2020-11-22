028f1c3b8a73afc17aebae9c7e9a772214672378
Paulo Nogueira <paulobh.nogueira@gmail.com>
2020-11-22 10:36:10 -0300

    Subindo o codigo do exercicio

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function carregar(){
+  const bodyConfig = getComputedStyle(document.body);
+  const bgBody     = bodyConfig.backgroundColor;
+  const container  = document.querySelector('.paragrafos');
+  console.log(bodyConfig);
+
+  for(let i = 0; i < 4; i++){
+    criaParagrafos('.paragrafos');
+  }
+
+  const paragrafos = container.querySelectorAll('p');
+  for(let paragrafo of paragrafos){
+    alteraCor(paragrafo, bgBody, '#FFF');
+  }
+}
+
+function criaParagrafos(elemento){
+  let paragrafos = document.querySelector(elemento);
+  let paragrafo  = document.createElement('p');
+  paragrafo.innerText = 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.' 
+    + 'Delectus distinctio dolor accusantium aspernatur illum. Incidunt,' 
+    + 'delectus non temporibus exercitationem iusto voluptate tenetur '
+    + 'rerum tempore, iste, eius quae atque illo dolor.'
+  ;
+  paragrafos.appendChild(paragrafo);
+}
+
+function alteraCor(paragrafo, bg, bf){
+  paragrafo.style.backgroundColor = bg;
+  paragrafo.style.color = bf;
+  paragrafo.style.textAlign = 'justify';
+  paragrafo.style.padding = '5px';
+}
+
+carregar();
